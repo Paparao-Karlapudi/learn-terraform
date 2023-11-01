e04c173d00d8af51a99ee6c048d49871c8fb860a
pappik <pappik@gmail.com>
2023-11-01 13:47:08 -0400

    parameters store testing

--- 02-data-sources/data.tf
+++ 02-data-sources/data.tf
@@ -7,3 +7,10 @@ data "aws_ami" "example" {
 output "ami" {
   value = data.aws_ami.example
 }
+
+data "aws_instance" "foo" {
+  instance_id = "i-0f23e50a18a7702c5"
+}
+output "instance-id" {
+  value = data.aws_instance.foo.public_ip
+}
