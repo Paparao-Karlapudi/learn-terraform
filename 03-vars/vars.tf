3e28c25713f10c6de9ba2f8a7dff2958194f259e
pappik <pappik@gmail.com>
2023-11-01 16:54:59 -0400

    parameters store testing

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -1,7 +1,52 @@
+# Default variable Type
 variable "sample" {
-  default = 10
+ default = 10
 }
 
 output "sample" {
   value = var.sample
 }
+#Strings
+variable "sample2" {
+  default = "rao"
+}
+output "sample2" {
+  value = var.sample2
+}
+#Boolean
+variable "sample3" {
+  default = true
+}
+
+output "sample3" {
+  value = var.sample3
+}
+#List type
+variable "sample4" {
+  default = [
+    "rao",
+    100,
+    true,
+    "fruit"
+    ]
+}
+
+output "sample4" {
+  value = var.sample4[0]
+}
+
+#Map Type
+variable "sample5" {
+  default = {
+    number  = 30,
+    boolean = true,
+    string  = "rao"
+  }
+  }
+output "sample5" {
+  value = var.sample5["number"]
+}
+
+
+
+
