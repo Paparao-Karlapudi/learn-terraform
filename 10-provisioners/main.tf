6ccd2245fca50dd82b8a2e5c5e6edba9ded7c864
pappik <pappik@gmail.com>
2023-11-02 19:03:34 -0400

    parameters store testing

---
+++ 10-provisioners/main.tf
@@ -0,0 +1,51 @@
+data "aws_ami" "example" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+resource "aws_instance" "test_instance" {
+  ami           = data.aws_ami.test_instance.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "terraform_test"
+  }
+
+  provisioner "remote_exec" {
+    connection {
+      host = self.public_ip
+      user = "centos"
+      password = "DevOps321"
+    }
+    inline = [
+    "false"
+    ]
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
