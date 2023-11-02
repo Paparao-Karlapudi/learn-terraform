d8421142fc418b41941be9647faaa0e91f281eaf
pappik <pappik@gmail.com>
2023-11-01 22:23:17 -0400

    parameters store testing

--- 05-remote-state/sample.tf
+++ 05-remote-state/sample.tf
@@ -11,7 +11,7 @@ data "aws_ami" "test_instance" {
 resource "aws_instance" "test_instance" {
   ami           = data.aws_ami.test_instance.id
   instance_type = "t3.micro"
-
+vpc_security_group_ids = [aws_security_group.allow_tls.id]
   tags = {
     Name = "terraform_test"
   }
@@ -22,5 +22,31 @@ terraform {
     bucket = "terraform-pappik"
     key    = "05-remote-state/terraform.tfstate"
     region = "us-east-1"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [0.0.0.0/0]
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
   }
 }
