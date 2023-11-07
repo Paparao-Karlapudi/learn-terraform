85c0e8188c806e40303647ad071a10ee88a9e1f4
pappik <pappik@gmail.com>
2023-11-07 15:11:20 -0500

    parameters store testing

--- 02-data-sources/data.tf
+++ 02-data-sources/data.tf
@@ -2,15 +2,8 @@ data "aws_ami" "example" {
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
   owners           = ["973714476881"]
-}
+    }
 
-output "ami" {
+output "ami_info" {
   value = data.aws_ami.example
-}
-
-data "aws_instance" "foo" {
-  instance_id = "i-0f23e50a18a7702c5"
-}
-output "instance-id" {
-  value = data.aws_instance.foo.public_ip
 }
